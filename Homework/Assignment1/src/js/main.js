df1f7a6c47ea3b887a022689f670d0c9b7955cc4
Mara Kinoff <maranicholemayfield@gmail.com>
2019-02-06 21:56:42 -0600

    rough draft of index complete

--- Homework/Assignment1/src/js/main.js
+++ Homework/Assignment1/src/js/main.js
@@ -1,4 +1,18 @@
 ReactDOM.render(
-    React.createElement("h1", {}, "Hello World"),
-    document.getElementById("root")
+    React.createElement(
+        "h1", 
+        {}, 
+        "Posh Pizza",
+        React.createElement(
+            "h5", 
+            {}, 
+            "Log In"
+        ),
+        React.createElement(
+            "h5", 
+            {}, 
+            "Create Profile"
+        )
+    ),
+    document.getElementById("top_header")
     );
