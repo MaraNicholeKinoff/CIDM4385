63557b171b5f8d77a21036d5e428ba4af66dbd13
Mara Mayfield <mara@run.biz>
2019-02-28 13:48:57 -0600

    adding basic code

--- InClass/02252019/pizza_bandit/src/App.js
+++ InClass/02252019/pizza_bandit/src/App.js
@@ -1,17 +1,58 @@
 import React, { Component } from 'react';
 import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
 import './App.css';
+import LoginForm from './Components/LoginForm'
+
+const Map = ReactMapboxGl({
+  accessToken: 'pk.eyJ1IjoiYWh1aW1hbnU2OSIsImEiOiJjanNpYnZnNG0wbTB6NDlxb3VqbzQ0ZjRpIn0.a4bjWgujDq3W3RxGVeuwVw',
+}); 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lng: -98.5795,
+      lat: 39.828175,
+      zoom: 2,
+    };
+
+    this.handleFormSubmission = this.handleFormSubmission.bind(this);
+    this.randomizeMapStyle = this.randomizeMapStyle.bind(this);
+  }
+
+  componentDidMount(){
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(position => {
+        this.setState( () => {
+            return {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            }
+          }
+        );
+      });
+    }else{
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }
+
+  handleFormSubmission(formdata){}
+
   render() {
+    const { lng, lat, zoom} = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          
-        </header>
-        <body>
-          
-        </body>
+      <div className="container">
+        <LoginForm onFormSubmit={this.handleFormSubmission} />
+        <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
+        <Map center={[lng, lat]} containerStyle={{ height: "400px", width: "100%" }}>
+          <Layer type="symbol"
+            id="marker"
+            layout={{ "icon-image": "marker-15"}}>
+            <Feature coordinates={[lng, lat]}/>
+          </Layer>
+        </Map>
       </div>
     );
   }
